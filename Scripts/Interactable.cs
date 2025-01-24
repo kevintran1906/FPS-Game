ea411ce34f058bfcc139f6b96368a5901aa2e8c7
kevintran1906 <99227006+kevintran1906@users.noreply.github.com>
2025-01-23 21:46:12 -0500

    Scripts used in Unity for movement controls. Movements include jumping, direction movements and perspective.

---
+++ Scripts/Interactable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    //message displayed to player when looking at an interactable.
+       public string promptMessage;
+
+
+       public void BaseInteract()
+       {
+
+       }
+
+       protected virtual void Interact()
+       {
+
+
+       }
+    
+   
+}
